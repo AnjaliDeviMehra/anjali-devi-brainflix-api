c8234a26eb0a666ff9838b55811b5bd1b8feadda
Anjali <anjalidevimehra@gmail.com>
2024-07-28 12:28:24 -0500

    add functionality to post a new video

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT;
+app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
--- routes/videos.js
+++ routes/videos.js
@@ -1,5 +1,6 @@
 import express from "express";
 import fs from "fs";
+import crypto from "crypto";
 const router = express.Router();
 
 const DATA_PATH = "./data/videos.json";
@@ -11,19 +12,55 @@ const getData = () => {
 
 router.get("/", (req, res) => {
   const videos = getData();
-  const videosList = videos.videoList;
-  res.status(200).send(videosList);
+  res.status(200).send(videos);
 });
 
 router.get("/:id", (req, res) => {
   const id = req.params.id;
-  const videos = getData().videoDetails;
+  const videos = getData();
   const requestedVideo = videos.find((video) => video.id === id);
   if (requestedVideo) {
     res.status(200).send(requestedVideo);
   } else {
     res.status(404).send("video not found");
   }
+});
+
+// router.post("/", (req, res) => {
+//   const albumObj = req.body;
+//   const newalbum = {
+//     id: crypto.randomUUID(),
+//     title: albumObj.title,
+//   };
+
+//   // const albumData = readData();
+//   // albumData.push(newalbum);
+//   // fs.writeFileSync(FILE_PATH, JSON.stringify(albumData));
+
+//   res.status(201).json(newalbum);
+// });
+
+// router.post("/", (req, res) => {
+//   console.log(req.body);
+//   const newVideo = {
+//     title: "title",
+//   };
+//   res.json(newVideo);
+// });
+
+router.post("/", (req, res) => {
+  const videoObject = req.body;
+  const newVideo = {
+    id: crypto.randomUUID(),
+    title: videoObject.title,
+    description: videoObject.description,
+  };
+
+  const videoData = getData();
+  videoData.push(newVideo);
+  fs.writeFileSync(DATA_PATH, JSON.stringify(videoData));
+
+  res.status(201).json(newVideo);
 });
 
 export default router;
